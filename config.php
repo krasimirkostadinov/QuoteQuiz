6668bfadd100e145e1f6725a3daf91be5ff3575c
Krasimir Kostadinov <krasimetala@gmail.com>
2015-08-04 11:40:47 +0300

    Fixed untracked files. Especially .gitignore

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Basic configuration of variables for including files
-define('ROOT_PATH', dirname(__FILE__));
-define('HOST_PATH', 'http://quiz.dev');
-
-//--------- Define database user and password
-define('DB_USER', '');
-define('DB_PASS', '');
